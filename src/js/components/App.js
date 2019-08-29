060ae44024bc63d86a308204a0183a5cd1d6e278
Seongchuel, Ahn <aciddust20@gmail.com>
2019-08-29 16:13:47 +0900

    update

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
 // 페이지 라우팅
-import { Link, Switch, Route, BrowserRouter as Router} from 'react-router-dom';
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './Home';
 import FragPage from './FragPage';
 import NotFound from './NotFound';
 const App = () => {
   return (
     <Router>
-      
-      <main>
+      <div>
         <Switch>
-          <Route path="/" component={Home} />
-          <Route path="/fragment" component={FragPage} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/fragment" component={FragPage} />
           <Route component={NotFound} />
         </Switch>
-      </main>
+      </div>
     </Router>
   );
 };
--- src/js/components/NavBar.js
+++ src/js/components/NavBar.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Link, Switch, Route, BrowserRouter as Router} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 const NavBar = () => {
     return (
       <React.Fragment>
         <ul>
           <li><Link to="/">Home</Link></li>
-          <li><Link to="/fragment">FragPage</Link></li>
-          <li><a href='https://aciddust.github.io/blog'>Contact</a></li>
+          <li><Link to="/fragment">Fragment</Link></li>
+          <li><a href="https://aciddust.github.io/blog">Contact</a></li>
         </ul>
       </React.Fragment>
     );
